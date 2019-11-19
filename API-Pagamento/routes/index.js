be36fdcb01413a1ababb8a4e6bb68bc7168a54c5
Rafael B <rafaborgui@gmail.com>
2019-11-19 00:06:59 -0300

    apis pagamento e viagens

--- API-Pagamento/routes/index.js
+++ API-Pagamento/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var dados = [];
 
-/* GET home page. */
+/* GET listas Pagamentos. */
 router.get('/pagamentos', function(req, res, next) {
-  res.json([{id:1, nome: 'Luiz', idade: 29},{id:2, nome: 'Fernando', idade: 29}]);
+  res.json(dados);
+});
+
+/* GET Pagamentos by ID. */
+router.get('/pagamentos/:id', function(req, res, next) {
+  let el = [];
+  dados.filter( function( elem, i, array ) {
+    if (elem.id == req.params.id)  {
+      el.push(elem);
+    }
+  });
+  res.json(el[0]);
+});
+
+/* Formato dos dados para o POST
+{
+	"id": 21,
+	"id_reserva":[
+		{"id": 5},
+		{"id":13}
+	],
+	"subtotal": 1.800,
+	"desconto": 200,
+	"valor": 1.600
+} */
+
+/* POST dos dados de Pagamento. */
+router.post('/pagamentos', function(req, res, next) {
+  dados.push(req.body);
+  res.json(dados);
 });
 
 module.exports = router;
+
